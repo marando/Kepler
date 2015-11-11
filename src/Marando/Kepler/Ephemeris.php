41ec3a14090ab1da0d91e4ea332a883eae9a7e75
Ashley Marando <a.marando@me.com>
2015-11-11 02:46:27 -0500

    documentation

--- src/Marando/Kepler/Ephemeris.php
+++ src/Marando/Kepler/Ephemeris.php
@@ -38,84 +38,85 @@ use \Marando\Units\Distance;
 use \Marando\Units\Time;
 
 /**
- * @property Horiz     $altaz         Apparent altitude and azimuth of the
- *                                    target centr. Adjusted for light-time,
- *                                    gravitational deflection of light, stellar
- *                                    aberration, precession and nutation
+ * Represents an collection of ephemeris results
  *
+ * @property Horiz     $altaz         Apparent altitude and azimuth of the target centr. Adjusted for lighime, gravitational deflection of light, stellar aberration, precession and nutation
  * @property SSObj     $center        Center body
- *
  * @property AstroDate $date          Date of observation
- *
- * @property Angle     $diam          Angular width of the target body assuming
- *                                    full disk visibility
- *
- * @property Distance  $dist          Target to observer apparent distance (as
- *                                    affected by light-time)
- *
- * @property Distance  $distTrue      Target to observer true distance (not
- *                                    affected by light-time)
- *
- * @property Eclip     $eclip         J2000.0 geometric heliocentric ecliptic
- *                                    longitude and latitude of target center
- *
+ * @property Angle     $diam          Angular width of the target body assuming full disk visibility
+ * @property Distance  $dist          Target to observer apparent distance (as affected by light-time)
+ * @property Distance  $distTrue      Target to observer true distance (not affected by light-time)
+ * @property Eclip     $eclip         J2000.0 geometric heliocentric ecliptic longitude and latitude of target center
  * @property Time      $gast          Greenwich apparent sidereal time
- *
- * @property float     $illum
- *
+ * @property float     $illum         Fraction of target illuminated by Sun as seen by observer
  * @property Time      $last          Local apparent sidereal time
- *
- * @property Time      $lightTime     1-way light-time from target center to
- *                                    observer
- *
+ * @property Time      $lightTime     1-way light-time from target center to observer
  * @property Geo       $location      Geographic observation location
- *
- * @property float     $phase
- *
- * @property Equat     $radec         J2000.0 astrometric right ascension and
- *                                    declination of target center. Adjusted
- *                                    for light-time.
- *
- * @property Equat     $radecApparent Airless apparent right ascension and
- *                                    declination of the target center with
- *                                    respect to the Earth true equator and
- *                                    equinox of the date. Adjusted for
- *                                    light-time, gravitational deflection of
- *                                    light, stellar aberration, precession
- *                                    and nutation.
- *
+ * @property float     $phase         Target-observer-Sun phase angle
+ * @property Equat     $radec         J2000.0 astrometric right ascension and declination of target center. Adjusted for light-time.
+ * @property Equat     $radecApparent Airless apparent right ascension and declination of the target center with respect to the Earth true equator and equinox of the date. Adjusted for light-time, gravitational deflection of light, stellar aberration, precession and nutation.
  * @property SSObj     $target        Target body
- *
- * @property Cartesian $xyz           Astrometric ICRF/J2000.0 cartesian target
- *                                    to center position and velocity. Adjusted
- *                                    for light-time
- *
- * @property Cartesian $xyzTrue       True ICRF/J2000.0 cartesian target to
- *                                    center position and velocity (not adjusted
- *                                    for light-time)
+ * @property Cartesian $xyz           Astrometric ICRF/J2000.0 cartesian target to center position and velocity. Adjusted for light-time
+ * @property Cartesian $xyzTrue       True ICRF/J2000.0 cartesian target to center position and velocity (not adjusted for light-time)
  */
 class Ephemeris implements ArrayAccess, Iterator {
+  //----------------------------------------------------------------------------
+  // Constructors
+  //----------------------------------------------------------------------------
 
+  /**
+   * Creates a new Ephemeris instance
+   */
   public function __construct() {
 
   }
 
+  // // // Static
+
+  /**
+   * Creates a new ephemeris item. The provided parameters are used to
+   * automatically calculate additional observational quantities
+   *
+   * @param  SSObj     $target    Target object
+   * @param  SSObj     $center    Center object
+   * @param  Cartesian $pvTrue    Geometric (true) target -> observer cartesian
+   * @param  Cartesian $pvAstrom  Astrometric target -> observer cartesian
+   * @param  Time      $lightTime Light-time
+   * @param  Distance  $absDiam   Absolute target diameter
+   * @param  AstroDate $date      Observational date and time
+   * @param  Geo       $obsrv     Geographic Observation location
+   * @return static
+   */
   public static function item(SSObj $target, SSObj $center, Cartesian $pvTrue,
           Cartesian $pvAstrom, Time $lightTime, Distance $absDiam,
           AstroDate $date, Geo $obsrv = null) {
 
-    $item            = new static();
+    // Create new instance
+    $item = new static();
+
+    // Set properties
     $item->target    = $target;
     $item->center    = $center;
     $item->pvTrue    = $pvTrue;
     $item->pvAstrom  = $pvAstrom;
-    $item->lightTime = $lightTime->setUnit('min');
+    $item->lightTime = $lightTime->setUnit('min');  // Minutes preferred
     $item->absDiam   = $absDiam;
     $item->date      = $date;
     $item->obsrv     = $obsrv;
 
     return $item;
   }
+
+  //----------------------------------------------------------------------------
+  // Properties
+  //----------------------------------------------------------------------------
+  //
+  // <editor-fold desc="Properties" defaultstate="collapsed">
+  /**
+   * Array access container
+   * @var array
+   */
+  protected $items = [];
 
   /**
    * True geometric center to target cartesian position/velocity vector
@@ -155,22 +156,22 @@ class Ephemeris implements ArrayAccess, Iterator {
   protected $obsrv;
 
   /**
-   *
+   * Target JPL DE item
    * @var SSObj
    */
   protected $target;
 
   /**
-   *
+   * Center JPL DE item
    * @var SSObj
    */
   protected $center;
 
   /**
-   *
+   * Observation epoch
    * @var Epoch
    */
-  protected $epoch;
+  protected $obsEpoch;
 
   /**
    *
@@ -181,7 +182,7 @@ class Ephemeris implements ArrayAccess, Iterator {
   public function __get($name) {
     switch ($name) {
       case "altaz":
-        $radec        = $this->radec;
+        $radec        = $this->radec;  // Set topo location and find horiz
         $radec->obsrv = $this->obsrv;
         return $this->cache($name, $radec->toHoriz());
 
@@ -238,58 +239,102 @@ class Ephemeris implements ArrayAccess, Iterator {
     }
   }
 
-  // Cache values on retrieval for speed
+  // </editor-fold>
+  //
+  //----------------------------------------------------------------------------
+  // Functions
+  //----------------------------------------------------------------------------
+
+  /**
+   * Returns cached properties by key name, or sets them if they have not yet
+   * been cached
+   *
+   * @param  string $key
+   * @param  mixed  $value
+   * @return mixed
+   */
   protected function cache($key, $value) {
     if (key_exists($key, $this->cache))
-      return $this->cache[$key];
+      return $this->cache[$key];           // return cached
     else
-      return $this->cache[$key] = $value;
+      return $this->cache[$key] = $value;  // cache, then return
   }
 
-  protected static function diam(Distance $absDiam, Distance $dist) {
-    return Angle::arcsec(206265 * $absDiam->au / $dist->au);
-  }
-
+  /**
+   * Calculates the geometric (true) heliocentric ecliptic position of this
+   * instance's target
+   * @return Eclip
+   */
   protected function eclipHelio() {
+    // Get heliocentric position & velocity vector
     $de      = (new Reader())->jde($this->date->copy()->toTDB()->jd);
     $pvhelio = $de->position($this->target, SSObj::Sun());
 
+    // raw pv-vector -> cartesian
     $e = $this->cache('epoch', $this->date->toEpoch());
     $x = Distance::au($pvhelio[0]);
     $y = Distance::au($pvhelio[1]);
     $z = Distance::au($pvhelio[2]);
     $c = new Cartesian(Frame::ICRF(), $e, $x, $y, $z);
 
+    // xyz -> equat -> eclip
     return $c->toEquat()->toEclip();
   }
 
+  /**
+   * Calculates the phase angle of this instance's target (angle T-S-O)
+   * @return Angle
+   */
   protected function phase() {
     // Target -> Sun distance
     $r = $this->eclip->dist->au;
 
-    // Target -> Earth
+    // Target -> Earth distance
     $Δ = $this->radecApparent->dist->au;
 
-    // Earth -> Sun distance (R)
-    $s   = microtime(true);
+    // Earth -> Sun distance
     $pvh = [];
     $pvb = [];
     IAU::Epv00($this->date->jd, 0, $pvh, $pvb);
     $pvh = $pvh[0];
     $R   = sqrt($pvh[0] * $pvh[0] + $pvh[1] * $pvh[1] + $pvh[2] * $pvh[2]);
 
-    // Calculate target phase angle
+    // Calculate target's phase angle
     return Angle::rad(acos(($r ** 2 + $Δ ** 2 - $R ** 2) / (2 * $r * $Δ)));
   }
 
+  /**
+   * Calculates the illuminated disk fraction of this instance's target as seen
+   * by the observer
+   * @return float
+   */
   protected function illum() {
     return round((1 + cos($this->phase->rad)) / 2 * 100, 3);
   }
 
+  // // // Static
+
+  /**
+   * Calculates an angular diameter from an absolute diameter and observational
+   * distance
+   *
+   * @param  Distance $absDiam Absolute diameter
+   * @param  Distance $dist    Observational distance
+   * @return Angle
+   */
+  protected static function diam(Distance $absDiam, Distance $dist) {
+    return Angle::arcsec(206265 * $absDiam->au / $dist->au);
+  }
+
+  // // // Overrides
+
+  /**
+   * Represents this instance as a string
+   * @return string
+   */
   public function __toString() {
     $e   = new static();
     $str = "\n";
-
     foreach ($this->items as $e) {
       $y = $e->date->year;
       $m = $e->date->monthName(false);
@@ -298,18 +343,13 @@ class Ephemeris implements ArrayAccess, Iterator {
       $i = sprintf("%02.0f", $e->date->min);
       $s = sprintf("%02.0f", $e->date->sec);
 
-      //$str .= "$y-$m-$d $h:$i:$s  {$e->radecApparent}\n";
       $str .= "$e->date  {$e->radecApparent}\n";
     }
 
     return $str;
   }
 
-  /**
-   *
-   * @var static
-   */
-  protected $items = [];
+  // // // Interfaces
 
   public function offsetExists($offset) {
     return key_exists($offset, $this->items);
@@ -332,159 +372,6 @@ class Ephemeris implements ArrayAccess, Iterator {
 
   protected $position = 0;
 
-  /**
-   *
-   * @return static
-   */
-  public function current() {
-    return $this->items[$this->position];
-  }
-
-  public function key() {
-    return $this->position;
-  }
-
-  public function next() {
-    ++$this->position;
-  }
-
-  public function rewind() {
-    $this->position = 0;
-  }
-
-  public function valid() {
-    return isset($this->items[$this->position]);
-  }
-
-}
-
-class __Ephemeris implements ArrayAccess, Iterator {
-
-  public $dateUTC;
-  public $dateUT;
-  public $jdUTC;
-  public $jdUT;
-  public $target;
-  public $center;
-
-  /**
-   * Geometric true cartesian position and velocity of the target
-   * @var Cartesian
-   */
-  public $xyzTrue;
-
-  /**
-   * Astrometric cartesian position and velocity of the target (includes target
-   * to observer light-time aberration)
-   * @var Cartesian
-   */
-  public $xyzAstrom;
-
-  /**
-   * Geometric true target to observer distance
-   * @var Distance
-   */
-  public $distTrue;
-
-  /**
-   * Astrometric (apparent) target to observer distance (includes target to
-   * observer light-time aberration
-   * @var Distance
-   */
-  public $dist;
-
-  /**
-   * ICRF/J2000.0 astrometric right ascension and declination of target center
-   * adjusted for light-time
-   * @var Equat
-   */
-  public $radecAstrom;
-
-  /**
-   * Airless apparent right ascension and declination of target center with
-   * respect to the Earth true-equator and the meridian containing the Earth
-   * true equinox of date. Adjusted for light-time, gravitational deflection of
-   * light, stellar aberration, precession & nutation. Topographic if an
-   * observation location was supplied.
-   * @var Equat
-   */
-  public $radecApparent;
-
-  /**
-   * Airless apparent azimuth and elevation of target center. Adjusted for
-   * light-time, the gravitational deflection of light, stellar aberration,
-   * precession and nutation. Topographic if an observation location was
-   * supplied.
-   * @var Horiz
-   */
-  public $altaz;
-  public $eclipAstrom;
-  public $eclipApparent;
-
-  /**
-   * Local Apparent Sidereal Time. The angle measured westward in the body
-   * true-equator of-date plane from the meridian containing the body-fixed
-   * observer to the meridian containing the true Earth equinox (defined by
-   * intersection of the true Earth equator of date with the ecliptic of date).
-   * @var Time
-   */
-  public $sidereal;
-  public $diameter;
-  public $eclipHeliocentr;
-  public $lightTime;
-  public $galactic;
-  public $solarTime;
-  public $hourAngle;
-  /////
-
-  /**
-   *
-   * @var static
-   */
-  protected $items = [];
-
-  public function offsetExists($offset) {
-    return key_exists($offset, $this->items);
-  }
-
-  public function offsetGet($offset) {
-    return isset($this->items[$offset]) ? $this->items[$offset] : null;
-  }
-
-  public function offsetSet($offset, $value) {
-    if (is_null($offset))
-      $this->items[]        = $value;
-    else
-      $this->items[$offset] = $value;
-  }
-
-  public function offsetUnset($offset) {
-    unset($this->items[$offset]);
-  }
-
-  public function __toString() {
-    $i   = new static();
-    $str = '';
-
-    foreach ($this->items as $i) {
-      $str .= "\n"
-              . "JD $i->jdUT UT\t"
-              . "$i->radecAstrom"
-      //. "$i->radecApparent\t"
-      //. "$i->diameter\t"
-      //. "$i->lightTime";
-      ;
-    }
-
-    return $str;
-  }
-
-  protected $position = 0;
-
-  /**
-   *
-   * @return static
-   */
   public function current() {
     return $this->items[$this->position];
   }
